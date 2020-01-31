e846e7c022652a95c6bf8fee7a427cbbeac055dc
Sir Eric <echeungsk@gmail.com>
2020-01-31 16:34:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,20 +2,16 @@ require 'pp'
 
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-
   data.each do |key, value|
     value.each do |v, names|
       names.each do |names|
-        # if !pigeon_list[names]
-        #   pigeon_list[names] = {}
-        # end
-          
+        if !pigeon_list[names]
+          pigeon_list[names] = {}
+        end
         if !pigeon_list[names][key]
           pigeon_list[names][key] = []
         end
-        
         pigeon_list[names][key].push(v.to_s)
-        
       end
     end
   end
