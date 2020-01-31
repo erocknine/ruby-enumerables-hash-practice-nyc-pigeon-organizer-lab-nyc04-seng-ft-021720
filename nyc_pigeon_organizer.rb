ab9292c487795ed3db46459cd05db39b2c46a048
Sir Eric <echeungsk@gmail.com>
2020-01-31 16:31:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,10 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |v, names|
       names.each do |names|
-        if !pigeon_list[names]
-          pigeon_list[names] = {}
-        end
-          pp pigeon_list
+        # if !pigeon_list[names]
+        #   pigeon_list[names] = {}
+        # end
+          
         if !pigeon_list[names][key]
           pigeon_list[names][key] = []
         end
